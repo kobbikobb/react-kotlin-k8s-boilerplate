116aa0a96ed25a7f56c6185477bdfd2287542a58
Jakob Jónasson <jakob.jonasson@gmail.com>
2024-10-20 22:09:39 +0000

    API boilerplate init

---
+++ api/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+    id("io.ktor.plugin") version "2.3.0"
+}
+
+application {
+    mainClass.set("com.kobbikobb.ApiAppKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:2.3.0")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.0")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+}
---
+++ api/src/main/kotlin/com/kobbikobb/ApiApp.kt
@@ -0,0 +1,17 @@
+package com.kobbikobb
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Welcome to the boilerplate")
+            }
+        }
+    }.start(wait = true)
+}
